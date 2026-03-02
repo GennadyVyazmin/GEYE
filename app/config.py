c8cbf7b13d8887dd2f44bcf046e5ffcdf16e4b87
gennadijvazmin <admin@zczn.ru>
2026-03-02 14:47:40 +0300

    1.0

--- app/config.py
+++ app/config.py
@@ -4,6 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
+    server_ip: str = "127.0.0.1"
     app_host: str = "0.0.0.0"
     app_port: int = 8000
     rtsp_url: str = "rtsp://user:password@camera-ip:554/stream1"
